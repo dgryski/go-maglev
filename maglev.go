94c108cda72fa1e83f6ff3773062b4c06cf0df32
West Damron <wdamron@happymoney.com>
2018-07-14 16:27:00 -0700

    Cleanup Lookup method (better looking asm isn't actually faster)

--- maglev.go
+++ maglev.go
@@ -142,17 +142,7 @@ func New(names []string, partitions uint, args ...Option) *Table {
 }
 
 func (t *Table) Lookup(key uint64) string {
-	var node int
-	if len(t.assignments) == 0 {
-		goto notFound
-	}
-	node = int(t.assignments[key%uint64(len(t.assignments))])
-	if node < 0 || node >= len(t.names) {
-		goto notFound
-	}
-	return t.names[node]
-notFound:
-	return ""
+	return t.names[t.assignments[key%uint64(len(t.assignments))]]
 }
 
 func (t *Table) PartitionOwner(partition uint) string {
